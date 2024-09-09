66953e35990738da9cd6b0bff3cc886d30745f56
Lars Trieloff <lars@trieloff.net>
2024-09-09 09:14:29 +0200

    Add tools/oversight from helix-website

--- temp/scripts/indexing-test.js
+++
@@ -1,5 +0,0 @@
-document.documentElement.classList.add('indexing-test4');
-const meta = document.createElement('meta');
-meta.name = 'indexing-test';
-meta.content = 'indexing-test4';
-document.head.appendChild(meta);
