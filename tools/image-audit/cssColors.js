bbbf04a1a2aab3f3be157a879a44f0bf9a4c2cdd
fkakatie <ksexton@adobe.com>
2024-10-09 16:22:23 -0600

    feat: more complete color list

--- tools/image-audit/cssColors.js
+++ tools/image-audit/cssColors.js
@@ -1,81 +1,578 @@
 // eslint-disable-next-line import/prefer-default-export
 export const cssColors = [
-  { name: 'AliceBlue', rgb: [240, 248, 255], hsl: [208, 100, 97] },
-  { name: 'AntiqueWhite', rgb: [250, 235, 215], hsl: [34, 78, 91] },
-  { name: 'Aqua', rgb: [0, 255, 255], hsl: [180, 100, 50] },
-  { name: 'Aquamarine', rgb: [127, 255, 212], hsl: [160, 100, 75] },
-  { name: 'Azure', rgb: [240, 255, 255], hsl: [180, 100, 97] },
-  { name: 'Beige', rgb: [245, 245, 220], hsl: [60, 56, 91] },
-  { name: 'Bisque', rgb: [255, 228, 196], hsl: [33, 100, 88] },
-  { name: 'BlanchedAlmond', rgb: [255, 235, 205], hsl: [36, 100, 90] },
-  { name: 'BlueViolet', rgb: [138, 43, 226], hsl: [271, 76, 53] },
-  { name: 'Brown', rgb: [165, 42, 42], hsl: [0, 59, 41] },
-  { name: 'Burlywood', rgb: [222, 184, 135], hsl: [34, 57, 70] },
-  { name: 'CadetBlue', rgb: [95, 158, 160], hsl: [182, 25, 50] },
-  { name: 'Chartreuse', rgb: [127, 255, 0], hsl: [90, 100, 50] },
-  { name: 'Chocolate', rgb: [210, 105, 30], hsl: [25, 75, 47] },
-  { name: 'Coral', rgb: [255, 127, 80], hsl: [16, 100, 66] },
-  { name: 'CornflowerBlue', rgb: [100, 149, 237], hsl: [219, 79, 66] },
-  { name: 'Cornsilk', rgb: [255, 248, 220], hsl: [48, 100, 93] },
-  { name: 'Crimson', rgb: [220, 20, 60], hsl: [348, 83, 47] },
-  { name: 'DarkBlue', rgb: [0, 0, 139], hsl: [240, 100, 27] },
-  { name: 'DarkCyan', rgb: [0, 139, 139], hsl: [180, 100, 27] },
-  { name: 'DarkGoldenRod', rgb: [184, 134, 11], hsl: [43, 89, 38] },
-  { name: 'DarkGray', rgb: [169, 169, 169], hsl: [0, 0, 66] },
-  { name: 'DarkGreen', rgb: [0, 100, 0], hsl: [120, 100, 20] },
-  { name: 'DarkKhaki', rgb: [189, 183, 107], hsl: [56, 38, 58] },
-  { name: 'DarkMagenta', rgb: [139, 0, 139], hsl: [300, 100, 27] },
-  { name: 'DarkOliveGreen', rgb: [85, 107, 47], hsl: [82, 39, 30] },
-  { name: 'DarkOrange', rgb: [255, 140, 0], hsl: [33, 100, 50] },
-  { name: 'DarkOrchid', rgb: [153, 50, 204], hsl: [280, 61, 50] },
-  { name: 'DarkRed', rgb: [139, 0, 0], hsl: [0, 100, 27] },
-  { name: 'DarkSalmon', rgb: [233, 150, 122], hsl: [15, 72, 70] },
-  { name: 'DarkSeaGreen', rgb: [143, 188, 143], hsl: [120, 25, 65] },
-  { name: 'DarkSlateBlue', rgb: [72, 61, 139], hsl: [248, 39, 39] },
-  { name: 'DarkSlateGray', rgb: [47, 79, 79], hsl: [180, 25, 25] },
-  { name: 'DarkTurquoise', rgb: [0, 206, 209], hsl: [181, 100, 41] },
-  { name: 'DarkViolet', rgb: [148, 0, 211], hsl: [282, 100, 41] },
-  { name: 'DeepPink', rgb: [255, 20, 147], hsl: [328, 100, 54] },
-  { name: 'DeepSkyBlue', rgb: [0, 191, 255], hsl: [195, 100, 50] },
-  { name: 'DimGray', rgb: [105, 105, 105], hsl: [0, 0, 41] },
-  { name: 'DodgerBlue', rgb: [30, 144, 255], hsl: [210, 100, 56] },
-  { name: 'FireBrick', rgb: [178, 34, 34], hsl: [0, 68, 42] },
-  { name: 'FloralWhite', rgb: [255, 250, 240], hsl: [40, 100, 97] },
-  { name: 'ForestGreen', rgb: [34, 139, 34], hsl: [120, 61, 34] },
-  { name: 'Fuchsia', rgb: [255, 0, 255], hsl: [300, 100, 50] },
-  { name: 'Gainsboro', rgb: [220, 220, 220], hsl: [0, 0, 86] },
-  { name: 'GhostWhite', rgb: [248, 248, 255], hsl: [240, 100, 99] },
-  { name: 'Gold', rgb: [255, 215, 0], hsl: [51, 100, 50] },
-  { name: 'GoldenRod', rgb: [218, 165, 32], hsl: [43, 74, 49] },
-  { name: 'Gray', rgb: [128, 128, 128], hsl: [0, 0, 50] },
-  { name: 'GreenYellow', rgb: [173, 255, 47], hsl: [83, 100, 59] },
-  { name: 'HoneyDew', rgb: [240, 255, 240], hsl: [120, 100, 97] },
-  { name: 'HotPink', rgb: [255, 105, 180], hsl: [330, 100, 71] },
-  { name: 'IndianRed', rgb: [205, 92, 92], hsl: [0, 53, 58] },
-  { name: 'Indigo', rgb: [75, 0, 130], hsl: [275, 100, 25] },
-  { name: 'Ivory', rgb: [255, 255, 240], hsl: [60, 100, 97] },
-  { name: 'Khaki', rgb: [240, 230, 140], hsl: [54, 77, 75] },
-  { name: 'Lavender', rgb: [230, 230, 250], hsl: [240, 67, 94] },
-  { name: 'LavenderBlush', rgb: [255, 240, 245], hsl: [340, 100, 97] },
-  { name: 'LawnGreen', rgb: [124, 252, 0], hsl: [90, 100, 49] },
-  { name: 'LemonChiffon', rgb: [255, 250, 205], hsl: [54, 100, 90] },
-  { name: 'LightBlue', rgb: [173, 216, 230], hsl: [195, 53, 79] },
-  { name: 'LightCoral', rgb: [240, 128, 128], hsl: [0, 78, 72] },
-  { name: 'LightCyan', rgb: [224, 255, 255], hsl: [180, 100, 94] },
-  { name: 'LightGoldenRodYellow', rgb: [250, 250, 210], hsl: [60, 80, 90] },
-  { name: 'LightGreen', rgb: [144, 238, 144], hsl: [120, 73, 75] },
-  { name: 'LightGrey', rgb: [211, 211, 211], hsl: [0, 0, 83] },
-  { name: 'LightPink', rgb: [255, 182, 193], hsl: [351, 100, 86] },
-  { name: 'LightSalmon', rgb: [255, 160, 122], hsl: [17, 100, 74] },
-  { name: 'LightSeaGreen', rgb: [32, 178, 170], hsl: [177, 70, 41] },
-  { name: 'LightSkyBlue', rgb: [135, 206, 250], hsl: [203, 92, 75] },
-  { name: 'LightSlateGray', rgb: [119, 136, 153], hsl: [210, 14, 53] },
-  { name: 'LightSteelBlue', rgb: [176, 196, 222], hsl: [214, 41, 78] },
-  { name: 'LightYellow', rgb: [255, 255, 224], hsl: [60, 100, 94] },
-  { name: 'LimeGreen', rgb: [50, 205, 50], hsl: [120, 61, 50] },
-  { name: 'Linen', rgb: [250, 240, 230], hsl: [30, 67, 94] },
-  { name: 'Magenta', rgb: [255, 0, 255], hsl: [300, 100, 50] },
-  // next two are "special" cssColors that we use to indicate different states
-  { name: 'Transparency', rgb: [-255, -255, -255], hsl: [-255, -255, -255] },
-  { name: 'Unknown', rgb: [-255, -255, -255], hsl: [-255, -255, -255] },
+  {
+    name: 'Alice Blue',
+    rgb: [240, 248, 255],
+    hsl: [208, 100, 97],
+  }, {
+    name: 'Antique White',
+    rgb: [250, 235, 215],
+    hsl: [34, 78, 91],
+  }, {
+    name: 'Aqua',
+    rgb: [0, 255, 255],
+    hsl: [180, 100, 50],
+  }, {
+    name: 'Aquamarine',
+    rgb: [127, 255, 212],
+    hsl: [160, 100, 75],
+  }, {
+    name: 'Azure',
+    rgb: [240, 255, 255],
+    hsl: [180, 100, 97],
+  }, {
+    name: 'Beige',
+    rgb: [245, 245, 220],
+    hsl: [60, 56, 91],
+  }, {
+    name: 'Bisque',
+    rgb: [255, 228, 196],
+    hsl: [33, 100, 88],
+  }, {
+    name: 'Black',
+    rgb: [0, 0, 0],
+    hsl: [0, 0, 0],
+  }, {
+    name: 'Blanched Almond',
+    rgb: [255, 235, 205],
+    hsl: [36, 100, 90],
+  }, {
+    name: 'Blue',
+    rgb: [0, 0, 255],
+    hsl: [240, 100, 50],
+  }, {
+    name: 'Blue Violet',
+    rgb: [138, 43, 226],
+    hsl: [271, 76, 53],
+  }, {
+    name: 'Brown',
+    rgb: [165, 42, 42],
+    hsl: [0, 59, 41],
+  }, {
+    name: 'Burly Wood',
+    rgb: [222, 184, 135],
+    hsl: [34, 57, 70],
+  }, {
+    name: 'Cadet Blue',
+    rgb: [95, 158, 160],
+    hsl: [182, 25, 50],
+  }, {
+    name: 'Chartreuse',
+    rgb: [127, 255, 0],
+    hsl: [90, 100, 50],
+  }, {
+    name: 'Chocolate',
+    rgb: [210, 105, 30],
+    hsl: [25, 75, 47],
+  }, {
+    name: 'Coral',
+    rgb: [255, 127, 80],
+    hsl: [16, 100, 66],
+  }, {
+    name: 'Cornflower Blue',
+    rgb: [100, 149, 237],
+    hsl: [219, 79, 66],
+  }, {
+    name: 'Cornsilk',
+    rgb: [255, 248, 220],
+    hsl: [48, 100, 93],
+  }, {
+    name: 'Crimson',
+    rgb: [220, 20, 60],
+    hsl: [348, 83, 47],
+  }, {
+    name: 'Cyan',
+    rgb: [0, 255, 255],
+    hsl: [180, 100, 50],
+  }, {
+    name: 'Dark Blue',
+    rgb: [0, 0, 139],
+    hsl: [240, 100, 27],
+  }, {
+    name: 'Dark Cyan',
+    rgb: [0, 139, 139],
+    hsl: [180, 100, 27],
+  }, {
+    name: 'Dark Golden Rod',
+    rgb: [184, 134, 11],
+    hsl: [43, 89, 38],
+  }, {
+    name: 'Dark Gray',
+    rgb: [169, 169, 169],
+    hsl: [0, 0, 66],
+  }, {
+    name: 'Dark Green',
+    rgb: [0, 100, 0],
+    hsl: [120, 100, 20],
+  }, {
+    name: 'Dark Khaki',
+    rgb: [189, 183, 107],
+    hsl: [56, 38, 58],
+  }, {
+    name: 'Dark Magenta',
+    rgb: [139, 0, 139],
+    hsl: [300, 100, 27],
+  }, {
+    name: 'Dark Olive Green',
+    rgb: [85, 107, 47],
+    hsl: [82, 39, 30],
+  }, {
+    name: 'Dark Orange',
+    rgb: [255, 140, 0],
+    hsl: [33, 100, 50],
+  }, {
+    name: 'Dark Orchid',
+    rgb: [153, 50, 204],
+    hsl: [280, 61, 50],
+  }, {
+    name: 'Dark Red',
+    rgb: [139, 0, 0],
+    hsl: [0, 100, 27],
+  }, {
+    name: 'Dark Salmon',
+    rgb: [233, 150, 122],
+    hsl: [15, 72, 70],
+  }, {
+    name: 'Dark Sea Green',
+    rgb: [143, 188, 143],
+    hsl: [120, 25, 65],
+  }, {
+    name: 'Dark Slate Blue',
+    rgb: [72, 61, 139],
+    hsl: [248, 39, 39],
+  }, {
+    name: 'Dark Slate Gray',
+    rgb: [47, 79, 79],
+    hsl: [180, 25, 25],
+  }, {
+    name: 'Dark Turquoise',
+    rgb: [0, 206, 209],
+    hsl: [181, 100, 41],
+  }, {
+    name: 'Dark Violet',
+    rgb: [148, 0, 211],
+    hsl: [282, 100, 41],
+  }, {
+    name: 'Deep Pink',
+    rgb: [255, 20, 147],
+    hsl: [328, 100, 54],
+  }, {
+    name: 'Deep Sky Blue',
+    rgb: [0, 191, 255],
+    hsl: [195, 100, 50],
+  }, {
+    name: 'Dim Gray',
+    rgb: [105, 105, 105],
+    hsl: [0, 0, 41],
+  }, {
+    name: 'Dodger Blue',
+    rgb: [30, 144, 255],
+    hsl: [210, 100, 56],
+  }, {
+    name: 'Fire Brick',
+    rgb: [178, 34, 34],
+    hsl: [0, 68, 42],
+  }, {
+    name: 'Floral White',
+    rgb: [255, 250, 240],
+    hsl: [40, 100, 97],
+  }, {
+    name: 'Forest Green',
+    rgb: [34, 139, 34],
+    hsl: [120, 61, 34],
+  }, {
+    name: 'Fuchsia',
+    rgb: [255, 0, 255],
+    hsl: [300, 100, 50],
+  }, {
+    name: 'Gainsboro',
+    rgb: [220, 220, 220],
+    hsl: [0, 0, 86],
+  }, {
+    name: 'Ghost White',
+    rgb: [248, 248, 255],
+    hsl: [240, 100, 99],
+  }, {
+    name: 'Gold',
+    rgb: [255, 215, 0],
+    hsl: [51, 100, 50],
+  }, {
+    name: 'Golden Rod',
+    rgb: [218, 165, 32],
+    hsl: [43, 74, 49],
+  }, {
+    name: 'Gray',
+    rgb: [128, 128, 128],
+    hsl: [0, 0, 50],
+  }, {
+    name: 'Green',
+    rgb: [0, 128, 0],
+    hsl: [120, 100, 25],
+  }, {
+    name: 'Green Yellow',
+    rgb: [173, 255, 47],
+    hsl: [83, 100, 59],
+  }, {
+    name: 'Honey Dew',
+    rgb: [240, 255, 240],
+    hsl: [120, 100, 97],
+  }, {
+    name: 'Hot Pink',
+    rgb: [255, 105, 180],
+    hsl: [330, 100, 71],
+  }, {
+    name: 'Indian Red',
+    rgb: [205, 92, 92],
+    hsl: [0, 53, 58],
+  }, {
+    name: 'Indigo',
+    rgb: [75, 0, 130],
+    hsl: [275, 100, 25],
+  }, {
+    name: 'Ivory',
+    rgb: [255, 255, 240],
+    hsl: [60, 100, 97],
+  }, {
+    name: 'Khaki',
+    rgb: [240, 230, 140],
+    hsl: [54, 77, 75],
+  }, {
+    name: 'Lavender',
+    rgb: [230, 230, 250],
+    hsl: [240, 67, 94],
+  }, {
+    name: 'Lavender Blush',
+    rgb: [255, 240, 245],
+    hsl: [340, 100, 97],
+  }, {
+    name: 'Lawn Green',
+    rgb: [124, 252, 0],
+    hsl: [90, 100, 49],
+  }, {
+    name: 'Lemon Chiffon',
+    rgb: [255, 250, 205],
+    hsl: [54, 100, 90],
+  }, {
+    name: 'Light Blue',
+    rgb: [173, 216, 230],
+    hsl: [195, 53, 79],
+  }, {
+    name: 'Light Coral',
+    rgb: [240, 128, 128],
+    hsl: [0, 79, 72],
+  }, {
+    name: 'Light Cyan',
+    rgb: [224, 255, 255],
+    hsl: [180, 100, 94],
+  }, {
+    name: 'Light Golden Rod Yellow',
+    rgb: [250, 250, 210],
+    hsl: [60, 80, 90],
+  }, {
+    name: 'Light Gray',
+    rgb: [211, 211, 211],
+    hsl: [0, 0, 83],
+  }, {
+    name: 'Light Green',
+    rgb: [144, 238, 144],
+    hsl: [120, 73, 75],
+  }, {
+    name: 'Light Pink',
+    rgb: [255, 182, 193],
+    hsl: [351, 100, 86],
+  }, {
+    name: 'Light Salmon',
+    rgb: [255, 160, 122],
+    hsl: [17, 100, 74],
+  }, {
+    name: 'Light Sea Green',
+    rgb: [32, 178, 170],
+    hsl: [177, 70, 41],
+  }, {
+    name: 'Light Sky Blue',
+    rgb: [135, 206, 250],
+    hsl: [203, 92, 76],
+  }, {
+    name: 'Light Slate Gray',
+    rgb: [119, 136, 153],
+    hsl: [210, 14, 53],
+  }, {
+    name: 'Light Steel Blue',
+    rgb: [176, 196, 222],
+    hsl: [214, 41, 78],
+  }, {
+    name: 'Light Yellow',
+    rgb: [255, 255, 224],
+    hsl: [60, 100, 94],
+  }, {
+    name: 'Lime',
+    rgb: [0, 255, 0],
+    hsl: [120, 100, 50],
+  }, {
+    name: 'LimeGreen',
+    rgb: [50, 205, 50],
+    hsl: [120, 61, 50],
+  }, {
+    name: 'Linen',
+    rgb: [250, 240, 230],
+    hsl: [30, 67, 94],
+  }, {
+    name: 'Magenta',
+    rgb: [255, 0, 255],
+    hsl: [300, 100, 50],
+  }, {
+    name: 'Maroon',
+    rgb: [128, 0, 0],
+    hsl: [0, 100, 25],
+  }, {
+    name: 'Medium Aqua Marine',
+    rgb: [102, 205, 170],
+    hsl: [160, 51, 60],
+  }, {
+    name: 'Medium Blue',
+    rgb: [0, 0, 205],
+    hsl: [240, 100, 40],
+  }, {
+    name: 'Medium Orchid',
+    rgb: [186, 85, 211],
+    hsl: [288, 59, 58],
+  }, {
+    name: 'Medium Purple',
+    rgb: [147, 112, 219],
+    hsl: [260, 60, 65],
+  }, {
+    name: 'Medium Sea Green',
+    rgb: [60, 179, 113],
+    hsl: [147, 50, 47],
+  }, {
+    name: 'Medium Slate Blue',
+    rgb: [123, 104, 238],
+    hsl: [249, 80, 67],
+  }, {
+    name: 'Medium Spring Green',
+    rgb: [0, 250, 154],
+    hsl: [157, 100, 49],
+  }, {
+    name: 'Medium Turquoise',
+    rgb: [72, 209, 204],
+    hsl: [178, 60, 55],
+  }, {
+    name: 'Medium Violet Red',
+    rgb: [199, 21, 133],
+    hsl: [322, 81, 43],
+  }, {
+    name: 'Midnight Blue',
+    rgb: [25, 25, 112],
+    hsl: [240, 64, 27],
+  }, {
+    name: 'Mint Cream',
+    rgb: [245, 255, 250],
+    hsl: [150, 100, 98],
+  }, {
+    name: 'Misty Rose',
+    rgb: [255, 228, 225],
+    hsl: [6, 100, 94],
+  }, {
+    name: 'Moccasin',
+    rgb: [255, 228, 181],
+    hsl: [38, 100, 85],
+  }, {
+    name: 'Navajo White',
+    rgb: [255, 222, 173],
+    hsl: [36, 100, 84],
+  }, {
+    name: 'Navy',
+    rgb: [0, 0, 128],
+    hsl: [240, 100, 25],
+  }, {
+    name: 'Old Lace',
+    rgb: [253, 245, 230],
+    hsl: [39, 85, 95],
+  }, {
+    name: 'Olive',
+    rgb: [128, 128, 0],
+    hsl: [60, 100, 25],
+  }, {
+    name: 'Olive Drab',
+    rgb: [107, 142, 35],
+    hsl: [80, 60, 35],
+  }, {
+    name: 'Orange',
+    rgb: [255, 165, 0],
+    hsl: [39, 100, 50],
+  }, {
+    name: 'Orange Red',
+    rgb: [255, 69, 0],
+    hsl: [16, 100, 50],
+  }, {
+    name: 'Orchid',
+    rgb: [218, 112, 214],
+    hsl: [302, 59, 65],
+  }, {
+    name: 'Pale Golden Rod',
+    rgb: [238, 232, 170],
+    hsl: [55, 67, 80],
+  }, {
+    name: 'Pale Green',
+    rgb: [152, 251, 152],
+    hsl: [120, 93, 79],
+  }, {
+    name: 'Pale Turquoise',
+    rgb: [175, 238, 238],
+    hsl: [180, 65, 81],
+  }, {
+    name: 'Pale Violet Red',
+    rgb: [219, 112, 147],
+    hsl: [340, 60, 65],
+  }, {
+    name: 'Papaya Whip',
+    rgb: [255, 239, 213],
+    hsl: [37, 100, 92],
+  }, {
+    name: 'Peach Puff',
+    rgb: [255, 218, 185],
+    hsl: [28, 100, 86],
+  }, {
+    name: 'Peru',
+    rgb: [205, 133, 63],
+    hsl: [30, 59, 53],
+  }, {
+    name: 'Pink',
+    rgb: [255, 192, 203],
+    hsl: [350, 100, 88],
+  }, {
+    name: 'Plum',
+    rgb: [221, 160, 221],
+    hsl: [300, 47, 75],
+  }, {
+    name: 'Powder Blue',
+    rgb: [176, 224, 230],
+    hsl: [187, 52, 80],
+  }, {
+    name: 'Purple',
+    rgb: [128, 0, 128],
+    hsl: [300, 100, 25],
+  }, {
+    name: 'Rebecca Purple',
+    rgb: [102, 51, 153],
+    hsl: [270, 50, 40],
+  }, {
+    name: 'Red',
+    rgb: [255, 0, 0],
+    hsl: [0, 100, 50],
+  }, {
+    name: 'Rosy Brown',
+    rgb: [188, 143, 143],
+    hsl: [0, 25, 65],
+  }, {
+    name: 'Royal Blue',
+    rgb: [65, 105, 225],
+    hsl: [225, 73, 57],
+  }, {
+    name: 'Saddle Brown',
+    rgb: [139, 69, 19],
+    hsl: [25, 76, 31],
+  }, {
+    name: 'Salmon',
+    rgb: [250, 128, 114],
+    hsl: [6, 93, 71],
+  }, {
+    name: 'Sandy Brown',
+    rgb: [244, 164, 96],
+    hsl: [28, 87, 67],
+  }, {
+    name: 'Sea Green',
+    rgb: [46, 139, 87],
+    hsl: [146, 50, 36],
+  }, {
+    name: 'Sea Shell',
+    rgb: [255, 245, 238],
+    hsl: [25, 100, 97],
+  }, {
+    name: 'Sienna',
+    rgb: [160, 82, 45],
+    hsl: [19, 56, 40],
+  }, {
+    name: 'Silver',
+    rgb: [192, 192, 192],
+    hsl: [0, 0, 75],
+  }, {
+    name: 'Sky Blue',
+    rgb: [135, 206, 235],
+    hsl: [197, 71, 73],
+  }, {
+    name: 'Slate Blue',
+    rgb: [106, 90, 205],
+    hsl: [248, 53, 58],
+  }, {
+    name: 'Slate Gray',
+    rgb: [112, 128, 144],
+    hsl: [210, 13, 50],
+  }, {
+    name: 'Snow',
+    rgb: [255, 250, 250],
+    hsl: [0, 100, 99],
+  }, {
+    name: 'Spring Green',
+    rgb: [0, 255, 127],
+    hsl: [150, 100, 50],
+  }, {
+    name: 'Steel Blue',
+    rgb: [70, 130, 180],
+    hsl: [207, 44, 49],
+  }, {
+    name: 'Tan',
+    rgb: [210, 180, 140],
+    hsl: [34, 44, 69],
+  }, {
+    name: 'Teal',
+    rgb: [0, 128, 128],
+    hsl: [180, 100, 25],
+  }, {
+    name: 'Thistle',
+    rgb: [216, 191, 216],
+    hsl: [300, 24, 80],
+  }, {
+    name: 'Tomato',
+    rgb: [255, 99, 71],
+    hsl: [9, 100, 64],
+  }, {
+    name: 'Turquoise',
+    rgb: [64, 224, 208],
+    hsl: [174, 72, 56],
+  }, {
+    name: 'Violet',
+    rgb: [238, 130, 238],
+    hsl: [300, 76, 72],
+  }, {
+    name: 'Wheat',
+    rgb: [245, 222, 179],
+    hsl: [39, 77, 83],
+  }, {
+    name: 'White',
+    rgb: [255, 255, 255],
+    hsl: [0, 0, 100],
+  }, {
+    name: 'White Smoke',
+    rgb: [245, 245, 245],
+    hsl: [0, 0, 96],
+  }, {
+    name: 'Yellow',
+    rgb: [255, 255, 0],
+    hsl: [60, 100, 50],
+  }, {
+    name: 'Yellow Green',
+    rgb: [154, 205, 50],
+    hsl: [80, 61, 50],
+  },
+  // two are 'special' colors to indicate different states
+  {
+    name: 'Transparent',
+    rgb: [-255, -255, -255],
+    hsl: [-255, -255, -255],
+  }, {
+    name: 'Unknown',
+    rgb: [-255, -255, -255],
+    hsl: [-255, -255, -255],
+  },
 ];
