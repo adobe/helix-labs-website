348e098c20226f3a73881f9edce66d876c264326
David Nuescheler <david@nuescheler.ch>
2025-03-22 15:44:05 -0700

    chore: lint

--- tools/snapshot-admin/401.js
+++ tools/snapshot-admin/401.js
@@ -61,7 +61,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const encoder = new TextEncoder();
     const data = encoder.encode(message);
     const hashBuffer = await window.crypto.subtle.digest('SHA-256', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer)); 
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hash = hashArray
       .map((b) => b.toString(16).padStart(2, '0'))
       .join('');
